38ce39385d48e080eeaed86b010fe31d52971f90
sudharsh2000 <78167348+sudharsh2000@users.noreply.github.com>
2025-06-22 16:13:58 +0530

    Update settings.py
    static file

--- zshop/settings.py
+++ zshop/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
