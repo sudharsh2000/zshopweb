0567477fefe2134cfdaf34c41572b4c72adeae33
sudharsh2000 <78167348+sudharsh2000@users.noreply.github.com>
2025-06-22 19:24:02 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
 
+# Collect static files
+python manage.py collectstatic --no-input --clear
+
+# Apply database migrations
+python manage.py migrate
